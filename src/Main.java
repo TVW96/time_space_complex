b2514f9e62623d1be980144271a0eb9a379b1b0c
Troy Washington <troyvw96@gmail.com>
2024-10-07 22:51:00 -0700

    code comments

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,46 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] intArray = new int[10];
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = i;
+        }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // instantiating the main instance
+        Main mainInstance = new Main();
+        // Printing string of arrays to console
+        System.out.println(Arrays.toString(intArray));
+        // Running the print pairs function and printing all possible unique pairs.
+        mainInstance.printPairs(intArray);
+    }
+
+    void printPairs ( int[] array){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                System.out.println(array[i] + ", " + array[j]);
+            }
         }
     }
 }
+
+// The outer loop i runs n times
+// while the number of iterations in the inner loop depends on the current value of n.
+// Total iterations = (n-1) + (n-2) + ... + 1 + 0 = n(n-1)/2
+
+// Time complexity = O(n^2)
+
+// The output of algorithm here uses a fixed amount of space provided to loop variables i and j,
+// The output space used to print each pair is not counted in space complexity because it does not grow
+// with input size, in terms of storage.
+
+// Space complexity = O(1)
+
+// For the specific use case of printing all unique pairs within an array O(n^2) is inherent
+// because we need to consider every combination.
+
+// ** consider possiblities of duplicate numbers in array. In this case a hash set would be needed.
