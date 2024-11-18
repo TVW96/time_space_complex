4495ee09b3188b36015d893ffef10530e9565f9a
Troy Washington <troyvw96@gmail.com>
2024-11-17 18:43:12 -0800

    complete analysis

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,55 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
-import java.util.Arrays;
-import java.util.Random;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
-        int[] intArray = new int[10];
+        // generate integer array {1, 2, 3}
+        int[] intArray = new int[3];
         for (int i = 0; i < intArray.length; i++) {
-            intArray[i] = i;
+            intArray[i] = i+1;
         }
+        // Run the code snippet printPairs
+        // int intArray = {1, 2, 3}
+        Main main = new Main();
+        main.printPairs(intArray);
+        // prints value pairs:
+        // 1, 2
+        // 1, 3
+        // 2, 3
 
-        // instantiating the main instance
-        Main mainInstance = new Main();
-        // Printing string of arrays to console
-        System.out.println(Arrays.toString(intArray));
-        // Running the print pairs function and printing all possible unique pairs.
-        mainInstance.printPairs(intArray);
+        // Time Complexity:
+        // O(n^2)
+        // (n * (n-1)) / 2
+        // (3 * (2)) / 2 = 3
+
+        // Space Complexity:
+        // O(1)
+
+        // generate String array of fruits
+        String[] Fruits = {"banana", "apple", "orange", "strawberry"};
+        // print unique value pairs of Fruits array
+        main.printStringPairs(Fruits);
+        // prints value pairs:
+        // banana, apple
+        // banana, orange
+        // banana, strawberry
+        // apple, orange
+        // apple, strawberry
+        // orange, strawberry
+
+        // Time Complexity:
+        // O(n^2)
+        // (n * (n-1)) / 2
+        // (4 * (3)) / 2 = 6
+
+        // Space Complexity:
+        // O(1)
+
+
+        //For checking duplicate values
+        int[] checkArray = {1, 2, 2, 2, 2, 3, 4};
+        main.printNoDuplicatePairs(checkArray);
     }
 
     void printPairs ( int[] array){
@@ -26,21 +59,26 @@ public class Main {
             }
         }
     }
+    // String representation of printPairs
+    void printStringPairs ( String[] array){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                System.out.println(array[i] + ", " + array[j]);
+            }
+        }
+    }
+    // HashSet with no duplicate values
+    void printNoDuplicatePairs(int[] array) {
+        HashSet<String> printedPairs = new HashSet<>();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                String pair = array[i] + ", " + array[j];
+                // check hash for duplicate values
+                if (!printedPairs.contains(pair)) {
+                    System.out.println(pair);
+                    printedPairs.add(pair);
+                }
+            }
+        }
+    }
 }
-
-// The outer loop i runs n times
-// while the number of iterations in the inner loop depends on the current value of n.
-// Total iterations = (n-1) + (n-2) + ... + 1 + 0 = n(n-1)/2
-
-// Time complexity = O(n^2)
-
-// The output of algorithm here uses a fixed amount of space provided to loop variables i and j,
-// The output space used to print each pair is not counted in space complexity because it does not grow
-// with input size, in terms of storage.
-
-// Space complexity = O(1)
-
-// For the specific use case of printing all unique pairs within an array O(n^2) is inherent
-// because we need to consider every combination.
-
-// ** consider possiblities of duplicate numbers in array. In this case a hash set would be needed.
